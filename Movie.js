d8b348ad12a2895f332a8802953e303c1570ee69
Kristiana Stevens <ks15j@my.fsu.edu>
2018-08-02 23:34:14 +0000

    lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=3f24ea64648e46c2ae18e768e2129f8a',
                 data: query
             })
             
@@ -56,8 +56,7 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/                
-                
-                
+                $("#overview").html(json.results[0].overview);
                 
 
                 /***********************Retrieve Movie Release Date from Server & Add to HTML Document***********
@@ -72,7 +71,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/               
 
-
+                $("#release_date").html(json.results[0].release_date);
 
 
                  /***********************Retrieve Movie Popularity Rate from Server & Add to HTML Document******
@@ -87,8 +86,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/   
 
-  
-
+                $("#popularity").html(json.results[0].popularity);
                 
                  /*****************Retrieve Movie Original Language from Server & Add to HTML Document***********
                  TASK #9:  
@@ -102,7 +100,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                 
 
-
+                $("#original_language").html(json.results[0].original_language);
 
                 //The following code retrieves the movie poster path from the JSON object retrieved from the TMDB 
                 //Server and places the poster image on the HTML page, where the id="poster_path".  It took TWO
--- README.md
+++ README.md
@@ -4,19 +4,29 @@
 LIS 2360:  Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+August, 01, 2018
 
 ### Student Name:  
-(First and Last Name)
+Kristiana Stevens
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+In this project, we create a movie review website similar to IMDb. This website
+uses the IMDb API in order to generate it's data.
 
 ### View Project:
 (Replace this statement with your Github Page URL that was created when you 
  published the project.)
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1.	AJAX – Ajax allows client web pages to quickly exchange data with a web server.
+    Additionally, it doesn’t even necessitate a reload of the webpage.
+    Ajax relies on the use of a programming language such as JavaScript as well as a 
+    data interchange format. There are some limitations when using Ajax. For instance,  
+    requested data can be located on a third-party server, however,
+    not all web servers or browsers support this.  
+2. 	JSON – JSON stands for JavaScript Object Notation. It’s a syntax for the storing 
+    and exchange of data. It’s a lightweight data interchange format, and a subset of JavaScript.
+    Additionally, it’s language independent and self-describing, therefore easy to understand. 
+3.	AJAX Security – AJAX-based web apps use the same security procedures as most web applications. 
+    You must specify authentication, authorization, and data protection requirements. Client-side, 
+    JavaScript is visible to the user, and hackers may use JavaScript to exploit server-side weaknesses. 
